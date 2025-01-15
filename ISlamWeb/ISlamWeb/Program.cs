c2ecf8e334b786fcc6d45747896e1cf2d4d6c5e8
ahmad zeiny <ahmadzeiny2@gmail.com>
2025-01-15 10:12:40 +0100

    Add files via upload

---
+++ ISlamWeb/ISlamWeb/Program.cs
@@ -0,0 +1,15 @@
+namespace ISlamWeb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.UseStaticFiles();
+            app.MapFallbackToFile("index.html");
+            app.Run();
+        }
+    }
+}
